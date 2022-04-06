75bc9ec8ea4006884c928bf21fe7482694e404b3
SimoneScapin <simonescap87@gmail.com>
2022-04-06 16:43:57 +0200

    modifiche alla card

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,14 @@
 <template>
-<div class="container-fluid my-container d-flex">
-    <input type="text" v-model.trim="stringToSearch"
-    @keyup.enter="$emit('newSearch' , stringToSearch)">  
+<div class="container-fluid my-container ">
+    <div class="row">
+        <div class="col-4">
+            <h1>Boolflix</h1>
+        </div>
+        <div class="col-4 my-input d-flex flex-column justify-content-center">
+            <input type="text" v-model.trim="stringToSearch"
+            @keyup.enter="$emit('newSearch' , stringToSearch)" placeholder="Cerca un titolo...">  
+        </div>   
+    </div>  
 </div>
 </template>
 <script>
@@ -17,6 +24,8 @@ data: function(){
 <style lang="scss">
 div.my-container{
     background-color: aliceblue;
-    height: 30px;
+}
+.my-input{
+    border: 2px solid red;
 }
 </style>
