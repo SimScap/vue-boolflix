2cef25b4494d353ef737b6637ef7b6d8049200c1
SimoneScapin <simonescap87@gmail.com>
2022-04-06 13:39:30 +0200

    commit dopo rottura commit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="container-fluid my-container d-flex">
+    <input type="text" v-model.trim="stringToSearch"
+    @keyup.enter="$emit('newSearch' , stringToSearch)">  
+</div>
+</template>
+<script>
+export default {
+name: 'HeaderIndex',
+data: function(){
+    return{
+        stringToSearch: '',
+    }
+}
+}
+</script>
+<style lang="scss">
+div.my-container{
+    background-color: aliceblue;
+    height: 30px;
+}
+</style>
